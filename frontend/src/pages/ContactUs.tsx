85c0e77b15ff2c6948449f7efa1ac3003a187bad
baratimanak-boop <baratimana.k@gmail.com>
2026-01-22 20:24:10 +0200

    Update ContactUs.tsx

--- frontend/src/pages/ContactUs.tsx
+++ frontend/src/pages/ContactUs.tsx
@@ -153,7 +153,7 @@ const ContactUs: React.FC = () => {
                   </div>
                   <div>
                     <p className="font-semibold text-blue-100 text-sm uppercase">Email</p>
-                    <a href="mailto:hr@jobinrwanda.com" className="text-sm hover:underline transition">Baratimana.k@gmail.com</a>
+                    <a href="baratimana.k@gmail.com" className="text-sm hover:underline transition">baratimana.k@gmail.com</a>
                   </div>
                 </div>
               </div>
